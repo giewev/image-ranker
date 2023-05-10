c354a7136e2217aabbb9e9f3b6f168f1ab919970
Ian Fade <giewev@gmail.com>
2023-05-10 15:56:40 -0600

    Make the matchmaking more complicated

--- elo_comparison_server.py
+++ elo_comparison_server.py
@@ -8,14 +8,32 @@ DB_PATH = 'mtg_card_art.db'
 app = Flask(__name__)
 
 
-def get_random_cards():
+def get_random_cards(randomness=100):
     conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
-    c.execute('SELECT id, name, art_url, elo, games_played FROM cards WHERE invalid=0 ORDER BY games_played, RANDOM() LIMIT 2')
-    cards = [{'id': row[0], 'name': row[1], 'art_url': row[2], 'elo':row[3], 'games_played':row[4]} for row in c.fetchall()]
-    conn.close()
-    return cards
+    c.execute('''
+        SELECT id, name, art_url, elo, games_played 
+        FROM cards 
+        WHERE invalid=0 
+        ORDER BY ((RANDOM() / CAST(-9223372036854775808 AS REAL)*?) + (1/(games_played + 0.01))/5 + (elo/1200)) desc
+        LIMIT 1'''
+        , (randomness,))
+    row = c.fetchone() 
+    first_card = {'id': row[0], 'name': row[1], 'art_url': row[2], 'elo':row[3], 'games_played':row[4]}
 
+    c.execute('''
+        SELECT id, name, art_url, elo, games_played 
+        FROM cards 
+        WHERE 1=1
+            AND invalid=0 
+            AND id != ?
+        ORDER BY ((RANDOM() / CAST(-9223372036854775808 AS REAL)*?) + (1/(games_played + 0.01))/5 - (abs(elo-?) / elo)) desc
+        LIMIT 1'''
+    , (first_card['id'], first_card['elo'], randomness))
+    row = c.fetchone() 
+    second_card = {'id': row[0], 'name': row[1], 'art_url': row[2], 'elo':row[3], 'games_played':row[4]}
+    conn.close()
+    return [first_card, second_card]
 
 def update_elo(winner_id, loser_id, k=64, report=False):
     conn = sqlite3.connect(DB_PATH)
@@ -68,7 +86,7 @@ def index():
 @app.route('/low_rank/', methods=['GET'])
 @app.route('/low_rank/<int:page>', methods=['GET'])
 def low_rank(page=1):
-    images_per_page = 10
+    images_per_page = 12
     conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
     c.execute('SELECT COUNT(*) FROM cards')
